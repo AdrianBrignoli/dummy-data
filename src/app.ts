5608b88a089c372b26b50e6c0efe59fc64a19315
Adrian Brignoli <brignoliadrian@gmail.com>
2025-02-11 15:41:14 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ dotenv.config();
 
 const app = express();
 
-// Middleware to log incoming requests
 app.use((req, res, next) => {
   console.log(`User connected: ${req.method} ${req.url}`);
   next(); // Proceed to the next middleware or route handler
