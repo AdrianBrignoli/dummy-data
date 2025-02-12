606bed6181711904373bf15cf81e9a60fd9fa406
Adrian Brignoli <brignoliadrian@gmail.com>
2025-02-12 15:46:19 +0100

    test

--- dist/app.js
+++ dist/app.js
@@ -2,24 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes';
-
+console.log('prodctroutes', productRoutes)
 dotenv.config();
-
 const app = express();
-
 app.use((req, res, next) => {
-  console.log(`User connected: ${req.method} ${req.url}`);
-  next(); // Proceed to the next middleware or route handler
+    console.log(`User connected: ${req.method} ${req.url}`);
+    next(); // Proceed to the next middleware or route handler
 });
-console.log('TJIHOOOOOOOOOOOOOOOOOOOOOOOOOO');
 app.use(express.json());
 app.use(cors());
-
 // Use API routes
 app.use('/api', productRoutes);
-
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000');
 });
-
 export default app;
